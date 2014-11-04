73c52a6536cae864d1ec9fa445ae68b0d6720fd5
Eben <eben.lowe@gmail.com>
2014-11-03 22:50:04 -0500

    Adding Google webfonts, layout to HAML.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,5 @@
 class PagesController < ApplicationController
 
-  def access
-    if params[:pin][:number] == ENV['ACCESS_PIN']
-      session[:access] = true
-      flash[:success] = 'Access Granted.'
-      redirect_to posts_path
-    else
-      flash[:alert] = 'Access Denied.'
-      redirect_to root_path
-    end
-  end
-
   def index
     @attempt = Attempt.new
   end
