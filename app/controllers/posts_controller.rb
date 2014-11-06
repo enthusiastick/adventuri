b68fb4786189c6bf48beee95a26742347a682116
Eben <eben.lowe@gmail.com>
2014-11-05 22:19:10 -0500

    Added create/index actions to post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,28 @@
 class PostsController < ApplicationController
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = 'Posted.'
+      redirect_to posts_path
+    else
+      flash[:alert] = 'Error.'
+      render :new
+    end
+  end
+
+  def index
+    @post = Post.last
+  end
+
   def new
     @post = Post.new
+  end
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:body, :title)
   end
 
 end
